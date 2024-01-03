cec0e8b5dec6824712c9231d29e4747f2f2d3b77
YolxanderHC <yolxander.jaca@humancontact.com>
2024-01-02 19:58:13 -0500

    first commit

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="hero">
+        <nav>
+            <div class="logo">
+                <p>A</p>
+                <p>Y</p>
+            </div>
+        </nav>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "LandingPage",
+
+}
+</script>
+
+<style scoped>
+    .hero{
+        height: 100vh;
+        width: 100vw;
+        background-color: black;
+    }
+
+    .logo{
+        width: 80px;
+        display: flex;
+    }
+    .logo p{
+        color: #FFF;
+        /*font-family: 'Gugi', sans-serif;*/
+        font-size: 80px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        letter-spacing: 10.48px;
+    }
+</style>
