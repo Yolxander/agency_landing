07a5713b040a5f3c0fdd338f8ad4a479578c885e
YolxanderHC <yolxander.jaca@humancontact.com>
2024-04-23 16:23:55 -0400

    testing email

--- src/views/AutoDownloadPdf.vue
+++ src/views/AutoDownloadPdf.vue
@@ -1,29 +1,16 @@
 <template>
     <div>
-        <!-- Invisible iframe for displaying the PDF -->
-        <iframe :src="pdfUrl" style="display:none;" @load="redirect"></iframe>
+        <!-- No visible content needed, as this page just triggers a download -->
     </div>
 </template>
 
 <script>
 export default {
-    name: 'AutoViewPdf',
-
-    data() {
-        return {
-            pdfUrl: 'SempreStudiosServices.pdf',  // Ensure the path is correct
-        };
-    },
-
-    methods: {
-        redirect() {
-            // Redirect to the PDF URL after the iframe has loaded
-            window.location.href = this.pdfUrl;
-        }
-    },
+    name: 'AutoDownloadPdf',
 
     mounted() {
-        // No actions needed here; the iframe's load event will handle the necessary actions
+        // Redirect to the PDF URL
+        window.location.href = 'SempreStudiosServices.pdf';  // Ensure the path is correct
     }
 }
 </script>
